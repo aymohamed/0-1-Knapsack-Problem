c56a4159350255d82103e87d50797874a2a98336
Abdijabar <aymohamed@middlebury.edu>
2017-04-01 13:27:38 -0400

    base case location fixed

--- KnapsackSolver.java
+++ KnapsackSolver.java
@@ -6,13 +6,12 @@ class KnapsackSolver {
 
     public static int knapSack(int W, int wt[], int vals[], int n){
         int myTable[][] = new int [n][W]; //creates a 2D array
-
+        if(W==0 | n==0){  //either the sack is empty or there is no item to take
+            myTable[i][j] = 0;
         for(int i=0; i<n; i++ ){
             for(int j=0; j<=W; j++ ){
                 int option1 = vals[i]+myTable[i-1][j-wt[i]];
                 int option2 = myTable[i-1][j];
-                if(W==0 | i==0){  //either the sack is empty or there is no item to take
-                    myTable[i][j] = 0;
                 if(wt[i]<=j){
                     myTable[i][j]= Math.max(option1, option2);
                 }else{
